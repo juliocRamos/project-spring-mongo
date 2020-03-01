5aa01ca31ace238c3a7ead30cf55a79faeff5d3d
Júlio C. Ramos <ps2.ramos@gmail.com>
2020-03-01 18:47:02 -0300

    Criação do padrão DTO para Usuarios
    Motivo: I/M (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Adicionado DTO para a entidade Usuario.
    
    Observações:
    
    Referência:

---
+++ src/main/java/com/learningspringmongo/dto/UsuarioDTO.java
@@ -0,0 +1,50 @@
+package com.learningspringmongo.dto;
+
+import java.io.Serializable;
+
+import com.learningspringmongo.entities.Usuario;
+
+public class UsuarioDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String id;
+
+	private String nome;
+
+	private String email;
+
+	public UsuarioDTO() {
+		super();
+	}
+
+	public UsuarioDTO(Usuario entity) {
+		this.id = entity.getId();
+		this.nome = entity.getNome();
+		this.email = entity.getEmail();
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+}
--- src/main/java/com/learningspringmongo/resources/UsuarioResource.java
+++ src/main/java/com/learningspringmongo/resources/UsuarioResource.java
@@ -1,6 +1,7 @@
 package com.learningspringmongo.resources;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.learningspringmongo.dto.UsuarioDTO;
 import com.learningspringmongo.entities.Usuario;
 import com.learningspringmongo.services.UsuarioServices;
 
@@ -18,9 +20,16 @@ public class UsuarioResource {
 	// Definição da minha classe de serviços.
 	@Autowired
 	private UsuarioServices service;
-	
-	@GetMapping
-	public ResponseEntity<List<Usuario>> findAll() {
+
+	@GetMapping(path = "/entities")
+	public ResponseEntity<List<Usuario>> findAllEntities() {
 		return ResponseEntity.ok().body(service.findAll());
+	}
+
+	@GetMapping(path = "/dtos")
+	public ResponseEntity<List<UsuarioDTO>> findAllDTOs() {
+		List<Usuario> entityList = service.findAll();
+		List<UsuarioDTO> dtoList = entityList.stream().map(e -> new UsuarioDTO(e)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(dtoList);
 	}
 }
