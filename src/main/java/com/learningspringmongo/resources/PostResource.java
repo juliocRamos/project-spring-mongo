8d5fd62d12b8f5d68841a206cca8125a39763adb
Júlio C. Ramos <ps2.ramos@gmail.com>
2020-03-03 20:36:01 -0300

    Implementado endpoit para obter Posts por id
    Motivo: I/M (M - Melhoria | C - Correção | I - Implementação)
    
    Descrição:
      - Adicionado novo endpoint para que sejam retornados os Posts
      utilizando o endpoit /posts/id_do_post.
    
    Observações:
    
    Referência:

---
+++ src/main/java/com/learningspringmongo/resources/PostResource.java
@@ -0,0 +1,26 @@
+package com.learningspringmongo.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.learningspringmongo.entities.Post;
+import com.learningspringmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+	@Autowired
+	private PostService service;
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post entity = service.findById(id);
+		return ResponseEntity.ok().body(entity);
+	}
+
+}
---
+++ src/main/java/com/learningspringmongo/services/PostService.java
@@ -0,0 +1,22 @@
+package com.learningspringmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.learningspringmongo.entities.Post;
+import com.learningspringmongo.repository.PostRepository;
+import com.learningspringmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repository;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repository.findById(id);
+		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto com id: " + id + " não encontrado"));
+	}
+}
